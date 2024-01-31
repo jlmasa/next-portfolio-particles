ea2e8536662613328fc6e5fa0a3c63d9d2b5d113
Jl Masa <johnlorenz.masa@gmail.com>
2024-01-31 10:28:31 +0800

    second commit

---
+++ config/particles.config.js
@@ -0,0 +1,90 @@
+const particlesConfig = {
+  autoPlay: true,
+  background: {
+    image: "",
+    position: "",
+    repeat: "",
+    size: "",
+    opacity: 1,
+  },
+  backgroundMask: {
+    composite: "destination-out",
+    cover: {
+      color: {
+        value: "#fff",
+      },
+      opacity: 0.5,
+    },
+    enable: false,
+  },
+  fullScreen: {
+    enable: true,
+    zIndex: 10,
+  },
+
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 1,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
+
+export default particlesConfig;
